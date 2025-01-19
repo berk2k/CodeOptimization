7d0cff59252ab0956841e82aad196f189920cd88
Berk Polat <berkpolat125@gmail.com>
2025-01-19 12:45:26 +0300

    example2 added

---
+++ Example2_Solution/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2_Solution.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException(int productId)
+            : base($"Product with ID {productId} not found.")
+        {
+
+        }
+    }
+}
