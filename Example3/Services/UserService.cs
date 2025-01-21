b385fad2d8128011c6da99aedc6598b335d10bf3
Berk Polat <berkpolat125@gmail.com>
2025-01-21 13:41:31 +0300

    ex3 fixed

---
+++ Example3/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Example3.Models;
+
+namespace Example3.Services
+{
+    public class UserService
+    {
+        public bool CreateUser(CreateUserRequestModel user)
+        {
+            // Saving to Storage
+            var created = true; // or false, depending on the result
+
+            return created;
+        }
+
+        public IEnumerable<UserViewModel> GetUsersAsync()
+        {
+            // Getting from Storage (Relational Db, Cache, NoSql etc.)
+            return new List<UserViewModel>
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Salih",
+                LastName = "Cantekin",
+                EmailAddress = "salihcantekin@gmail.com"
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Salih Can",
+                LastName = "tekin",
+                EmailAddress = "salihcan_tekin@gmail.com"
+            },
+        };
+        }
+    }
+}
