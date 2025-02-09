7c8813a2fe4151c10f319945c1e08f0adb862b19
Berk Polat <berkpolat125@gmail.com>
2025-02-09 14:28:56 +0300

    updated

---
+++ MissionSpan.Console/BenchMarks.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionSpan.Console
+{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net80)]
+
+    public class BenchMarks
+    {
+        private readonly string[] logLines;
+
+        public BenchMarks()
+        {
+            logLines = File.ReadLines("request_logs.txt").ToArray();
+        }
+
+        [Benchmark]
+        public List<LogModel> ProcessLogs_Span()
+        {
+            return LogParser.ProcessLogs_Span(logLines);
+        }
+
+        [Benchmark]
+        public List<LogModel> ProcessLogs_String()
+        {
+            return LogParser.ProcessLogs_String(logLines);
+        }
+
+
+        [Benchmark]
+        public List<LogModel> ProcessLogs_Split()
+        {
+            return LogParser.ProcessLogs_Split(logLines);
+        }
+
+        [Benchmark]
+        public List<LogModel> ProcessLogs_Regex()
+        {
+            return LogParser.ProcessLogs_Regex(logLines);
+        }
+
+        [Benchmark]
+        public List<LogModel> ProcessLogs_CompiledRegex()
+        {
+            return LogParser.ProcessLogs_CompiledRegex(logLines);
+        }
+    }
+}
