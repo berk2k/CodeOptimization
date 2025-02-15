d718fc6a34a672dc52d48ce73fad9442c2016b34
Berk Polat <berkpolat125@gmail.com>
2025-02-15 17:14:11 +0300

    programcs updated

--- OneOf.Api/Program.cs
+++ OneOf.Api/Program.cs
@@ -1,3 +1,5 @@
+using OneOf.Api.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.ConfigureExceptionHandling();
 
 app.UseHttpsRedirection();
 
