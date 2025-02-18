592ba8ea534704219ce52c1446817dcc298a25f6
Berk Polat <berkpolat125@gmail.com>
2025-02-18 16:06:28 +0300

    updated

---
+++ Example6/Exceptions/CustomAzureException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6.Exceptions
+{
+    internal class CustomAzureException(string message) : Exception(message)
+    {
+    }
+}
